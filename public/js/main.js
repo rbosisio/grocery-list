051b195a2c65ecbbccafe93ed5c418205cdf0e91
rbosisio <rod.bosisio@gmail.com>
2017-07-02 15:08:31 -0300

    Fixing partials in routeProvider

--- public/js/main.js
+++ public/js/main.js
@@ -2,12 +2,12 @@ angular.module('listhero', ['ngRoute'])
 	.config(function($routeProvider, $locationProvider) {
 
 		$routeProvider.when('/', {
-			templateUrl: 'templates/home.html',
+			templateUrl: 'partials/home.html',
 			controller: 'HomeController'
 		});
 
 		$routeProvider.when('/list/:id', {
-			templateUrl: 'templates/list.html',
+			templateUrl: 'partials/list.html',
 			controller: 'ListController'
 		});
 	});
